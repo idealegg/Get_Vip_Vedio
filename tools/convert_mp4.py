c438a741a97fb0d8b527d406a1f026f4b66e4778
idealegg <452144941@qq.com>
2021-05-11 11:00:24 +0800

    1. convert_mp4 v2

--- tools/convert_mp4.py
+++ tools/convert_mp4.py
@@ -22,32 +22,32 @@ def do_task(cmds):
 		print(cmd)
 		os.system(cmd)
 
-def split_mp4(mp4):
+def split_mp4(mp4_inf):
+	mp4 = mp4_inf['name']
 	cmd = "avconv -i %s 2>&1|grep Duration" % mp4
 	tmp = os.popen(cmd)
 	duration = 0
-	offset = 0
+	offset = mp4_inf['start']
+	endt = mp4_inf['end']
 	th_list = []
 	for line in tmp:
 		print(line)
 		if line.count("Duration:"):
 			res = re.search("Duration:\s*(\d{2}):(\d{2}):(\d{2}).(\d{2}),", line)
 			if res:
-				duration = int(res.group(1)) * 3600 + int(res.group(2)) * 60 + int(res.group(3)) + int(res.group(4))/100 
+				duration = int(res.group(1)) * 3600 + int(res.group(2)) * 60 + int(res.group(3)) + int(res.group(4))/100
 				print("Get a duration: %s" % duration)
-				break				
-	while duration > (offset+1)*max_dur:
-		slice_n = "".join([mp4.replace(".mp4", ''), '_', str(offset+1), ".mp4"])
-		slice_n2 = "".join([mp4.replace(".mp4", ''), '_', str(offset+1), "_2", ".mp4"])
-		cmd = "avconv -ss %s -i %s -c copy -t %s -y %s 2>&1" %(get_offset(offset*max_dur), mp4, max_dur, slice_n)
-		cmd1 = "avconv -i %s -vf transpose=1 -y %s 2>&1" %(slice_n, slice_n2)
-		cmd2 = "rm %s" % slice_n
-		th = threading.Thread(target=do_task, args=([cmd, cmd1, cmd2],))
-		th_list.append(th)
-		th.start()
-		offset += 1
-	for th in th_list:
-		th.join()
+				break
+	while duration - endt > offset:
+		index = str(offset // max_dur +1)
+		slice_n = "".join([mp4.replace(".mp4", ''), '_', index, ".mp4"])
+		slice_n2 = "".join([mp4.replace(".mp4", ''), '_', index, "_2", ".mp4"])
+		cmd = "avconv -i %s -ss %s -t %s -vf transpose=1 -y %s 2>&1" %(mp4, get_offset(offset), max_dur if ((duration - endt) > (offset + max_dur)) else int(duration - endt -offset), slice_n)
+		#cmd = "avconv -i %s -ss %s -t %s -c copy -y %s 2>&1" %(mp4, get_offset(offset), max_dur if ((duration - endt) > (offset + max_dur)) else int(duration - endt -offset), slice_n2)
+		#cmd1 = "avconv -i %s -vf transpose=1 -y %s 2>&1" %(slice_n2, slice_n)
+		#cmd2 = "rm %s" % slice_n2
+		do_task([cmd])
+		offset += max_dur
 
 def main(mp4s):
 	for mp4 in mp4s:
@@ -55,6 +55,6 @@ def main(mp4s):
 
 if __name__ == "__main__":
 	main([
-		u"09dota提高班召唤师卡尔的华丽舞步.mp4",
-		u"09的影魔体验1第一视角.mp4",
+		 {'name': u"【09dota超清提高班】纯爷们第一视角.mp4", 'start': 30 + max_dur * 0, 'end': 0},
+		 {'name': u"【09dota超清提高班】月之骑士.mp4", 'start': 22 + max_dur * 0, 'end': 0},
 		])
